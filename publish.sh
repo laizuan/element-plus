273805997dce6d27d8b226db9990e5b69b0371bb
laizuan <1032299142@qq.com>
2023-06-06 17:26:05 +0800

    chore: 从本地文件中获取npm 推送 token

--- publish.sh
+++ publish.sh
@@ -6,20 +6,20 @@ tar -zcvf element-plus.tar.gz ./packages/
 md5=($(md5sum element-plus.tar.gz))
 rm -rf ./element-plus.tar.gz
 
-read -p "请输入要发布的版本号 x.x.x-stable.x 格式  " input
+read -p "请输入要发布的版本号 x.x.x-stable.x 格式  " version
 
-export TAG_VERSION=$input
+export TAG_VERSION=$version
 export GIT_HEAD=$md5
 sh ./scripts/publish.sh
 
+authToken=$(cat ./.npmtoken)
 
 cd dist/element-plus
-
 
 cat > ./.npmrc << EOF
 registry=https://registry.npmjs.org/
 //registry.npmjs.org/:always-auth=true
-//registry.npmjs.org/:_authToken=
+//registry.npmjs.org/:_authToken=${authToken}
 EOF
 
 cat > ./.npmignore << EOF
