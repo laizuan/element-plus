8ba7b172244bdd103939c1b46be0d7def91e80de
laizuan <1032299142@qq.com>
2023-10-20 15:01:49 +0800

    ci: 合并完成后删除源分支

--- rebase.sh
+++ rebase.sh
@@ -5,5 +5,5 @@ read -p "需要合并的ElementPlus版本号：" version
 git checkout tags/$version -b v$version
 git checkout dev
 git core.quotepath=false -c log.showSignature=false merge v$version
-
+git branch -d v$version
 echo "合并完成，请确定无误后合并到main分支，并打包发布到仓库"
