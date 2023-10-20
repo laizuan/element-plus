a25ee90e628cd53aa6fdab7bafd623d0eb227e6e
laizuan <1032299142@qq.com>
2023-10-20 14:59:29 +0800

    ci: 改用merge合并功能

--- rebase.sh
+++ rebase.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-git checkout dev
 git fetch upstream
-git branch
 
 read -p "需要合并的ElementPlus版本号：" version
-git rebase $version
+git checkout tags/$version -b v$version
+git checkout dev
+git core.quotepath=false -c log.showSignature=false merge v$version
 
 echo "合并完成，请确定无误后合并到main分支，并打包发布到仓库"
